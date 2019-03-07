3d659cd13b1c3a3261ac9bcfbb518d27d4dc2213
Ad Friedman <adien330@yahoo.com>
2019-03-07 20:12:35 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,14 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+    :montague => { :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       },
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+      :patriarch => {}, 
+      :matriarch => {}, 
+      :heroine => {}, 
       :heroine_friends => []
       }
     }
