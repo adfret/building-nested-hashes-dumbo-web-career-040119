1bbf8c090b83734a7787999903579846e90d3552
Ad Friedman <adien330@yahoo.com>
2019-03-07 19:41:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
 :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet", :age => "50"
