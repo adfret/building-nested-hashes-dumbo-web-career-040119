ee7cc58af3df6045fa60a59718539426db9741a6
Ad Friedman <adien330@yahoo.com>
2019-03-07 19:39:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         :name => "Lord Capulet", :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet"
         },
       :heroine => {
         #your key/value pairs here
