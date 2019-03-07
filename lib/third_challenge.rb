d423d9a5298e5bbec78a1aba3a1bfbccf44f5910
Ad Friedman <adien330@yahoo.com>
2019-03-07 19:46:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,8 +9,7 @@ epic_tragedy = { :montague => {
     :patriarch => {name: "Lord Capulet", age: "50"},
     :matriarch => {name: "Lady Capulet", age: "51"},
     :heroine => {name: "Juliet", age: "15", status: "alive"},
-    :heroine_friends => []
- }
+    :heroine_friends => {} }
 }
 
 
